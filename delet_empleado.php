00b142b52ea0563fe4eeb2d5bac38d6c8446c00c
John Doedaniel <skaad102@gmail.com>
2021-03-16 13:37:27 -0500

    deler rol

--- delet_empleado.php
+++ delet_empleado.php
@@ -4,18 +4,21 @@ include('database/connection.php');
 if(isset($_GET['id'])){
     $id = $_GET['id'];
 
+
+    $delUserRol = $Con->prepare('DELETE FROM empleado_rol WHERE empleado_id = :id');
+    $delUserRol->bindparam(':id', $id);
+
+
     $delUser = $Con->prepare('DELETE FROM empleado WHERE id = :id');
     $delUser->bindparam(':id', $id);
 
 
-    if($delUser->execute()){
+    if($delUserRol->execute() && $delUser->execute()){
         $_SESSION['message'] = 'Empleado Eliminado';
         $_SESSION['message_type'] = 'danger';
     
         header('Location: index.php');
     }
-
-
 
 }
 
